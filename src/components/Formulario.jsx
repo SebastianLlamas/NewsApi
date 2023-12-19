c1e455578d13eecd77e913bd3a444ae880f32a18
sllamas <sllamas@octanesystems.mx>
2023-12-18 21:11:48 -0800

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,17 @@
+import {FormControl, InputLabel, Select, MenuItem, Button, Box, Slide} from '@mui/material'
+const Formulario = () => {
+    return (
+        <form>
+            <FormControl>
+                <InputLabel>Categoria</InputLabel>
+                <Select
+                    label="Categoria"
+                >
+                    
+                </Select>
+            </FormControl>
+        </form>
+    )
+}
+
+export default Formulario;
